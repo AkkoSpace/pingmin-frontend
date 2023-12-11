62ba2ad2387d72a907cd51447a6e6f20b2c109ee
yantingwei <akkoyan@qq.com>
2023-12-11 17:32:47 +0800

    :construction: feat: 系统页面 - 登录

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,11 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    NButton: typeof import('naive-ui')['NButton']
+    NCard: typeof import('naive-ui')['NCard']
+    NForm: typeof import('naive-ui')['NForm']
+    NFormItem: typeof import('naive-ui')['NFormItem']
+    NInput: typeof import('naive-ui')['NInput']
     README: typeof import('./components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/pages/system/login/index.vue
+++ src/pages/system/login/index.vue
@@ -1,16 +1,58 @@
 <script lang="ts" setup>
 const { t } = useI18n()
+const loginForm = ref({
+  userAccount: '',
+  userPassword: '',
+})
+const loginRules = ref({
+  userAccount: [
+    { required: true, message: t('systemLogin.userAccountMsg'), trigger: 'blur,change' },
+  ],
+  userPassword: [
+    { required: true, message: t('systemLogin.userPasswordMsg'), trigger: 'blur,change' },
+  ],
+})
+// const login = () => {
+//   const { username, password } = loginForm
+//   loginApi({ username, password }).then(res => {
+//     const { data } = res
+//     if (data.code === 200) {
+//       const { token } = data.data
+//       setToken(token)
+//       router.push({ path: '/' })
+//     }
+//   })
+// }
 </script>
 
 <template>
-  <div>
-    <h1>{{ t("system.login") }}</h1>
+  <div h-screen w-screen>
+    <div mx-auto h-full w-md flex items-center>
+      <n-card hoverable>
+        <n-form
+          :model="loginForm"
+          :rules="loginRules"
+          label-placement="left"
+          label-width="auto"
+        >
+          <n-form-item :label="$t('systemLogin.userAccount')" prop="userAccount">
+            <n-input v-model="loginForm.userAccount" :placeholder="$t('systemLogin.userAccountMsg')" />
+          </n-form-item>
+          <n-form-item :label="$t('systemLogin.userPassword')" prop="userPassword">
+            <n-input v-model="loginForm.userPassword" :placeholder="$t('systemLogin.userPasswordMsg')" />
+          </n-form-item>
+          <n-form-item>
+            <n-button round type="info">
+              {{ t('systemLogin.login') }}
+            </n-button>
+          </n-form-item>
+        </n-form>
+      </n-card>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
-
 <route lang="yaml">
 meta:
-  layout: auth
+layout: auth
 </route>
